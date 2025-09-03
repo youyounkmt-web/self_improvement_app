b15cd96cb2ab0c49ab5928130c6bb55c04c3f7fb
mizuki <miduki2004suemasa@icloud.com>
2025-09-04 00:30:00 +0900

    仮称変更３

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '自分磨き（仮）ハリボーチョコマシュマロ仕様',
+      title: '自分磨き（仮）ハリボーチョコマシュマログミッツェル仕様',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
